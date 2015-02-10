aa8155a0a4b9792107ad8155f16ec59028e2b4a4
Gennaro Tortone <gennaro.tortone@na.infn.it>
2015-02-10 23:08:27 +0900

    hall temperature from EPICS PV

---
+++ nodejs/epics.js
@@ -0,0 +1,40 @@
+/*
+ * Example showing how to use Beebotte to monitor an EPICS PV
+ *
+ * Copyright, gennaro.tortone@na.infn.it, 2015
+ * MIT license
+ */
+
+var epics = require('epics');
+var bbt = require('beebotte');
+
+var bclient = new bbt.Connector({
+  //API keys for your account
+  keyId: process.env.ACCESS_KEY,
+  secretKey: process.env.SECRET_KEY
+});
+
+//Frequency of activity reporting in milliseconds
+var frequency = process.env.FREQUENCY || (60 * 1000 /* 1 minute */);
+// Channel name. Change it as suits you (it MUST correspond to an existing channel in your account)
+var channel_name = "temperature";
+
+setInterval(function()
+  {
+    var pv = new epics.Channel('LTC2499:NTC:sh:degC');
+
+    pv.connect(function() {
+       pv.get(function(err, value) {
+       //console.log('Current:',value);
+       bclient.write({
+          channel: channel_name,
+          resource: "hall_temp",
+          data: value
+       }, function(err, res) {
+          if(err) console.log(err);
+       });
+    });
+})
+
+  }, frequency
+);
